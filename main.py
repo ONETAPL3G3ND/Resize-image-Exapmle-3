f71dfee796187430c1245526b71779ed65e19b88
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-06-10 22:14:41 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import cv2 
+src = cv2.imread('D:/cv2-resize-image-original.png', cv2.IMREAD_UNCHANGED) 
+# set a new height in pixels 
+new_height = 200 
+# dsize 
+dsize = (src.shape[1], new_height)
+# resize image 
+output = cv2.resize(src, dsize, interpolation = cv2.INTER_AREA) 
+cv2.imwrite('D:/cv2-resize-image-height.png',output)
+Источник: https://tonais.ru/library/izmenenie-razmera-izobrazheniya-opencv-cv2-v-python
